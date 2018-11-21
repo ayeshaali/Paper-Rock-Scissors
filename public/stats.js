fd18cc0af64568fe2df2a7d845d6f2d6d437c192
ayeshaali <aliayeshanyc@gmail.com>
2018-11-21 17:29:34 -0500

    message

--- public/stats.js
+++ public/stats.js
@@ -7,7 +7,7 @@ function printStats() {
   document.getElementById("Total Games").innerHTML = "Total Games: "+player_obj["Total Games"];
   document.getElementById("Total Wins").innerHTML = "Total Wins: "+player_obj["Total Wins"];
   document.getElementById("Win Loss Ratio").innerHTML = "Win Loss Ratio: "+parseFloat((player_obj["Win Loss Ratio"]).toFixed(2));
-  document.getElementById("player_stats").innerHTML = "Rock: "+player_obj["rock"]+"% Paper: "+player_obj["paper"]+"% Scissors: "+player_obj["scissors"];
-  document.getElementById("browser_stats").innerHTML = "Rock: "+npc_obj["rock"]+"% Paper: "+npc_obj["paper"]+"% Scissors: "+npc_obj["scissors"];
+  document.getElementById("player_stats").innerHTML = "Rock: "+(player_obj["rock"]/player_obj["Total Games"])*100+"% Paper: "+(player_obj["paper"]/player_obj["Total Games"])*100+"% Scissors: "+(player_obj["scissors"]/player_obj["Total Games"])*100+"%";
+  document.getElementById("browser_stats").innerHTML = "Rock: "+(npc_obj["rock"]/player_obj["Total Games"])*100+"% Paper: "+(npc_obj["paper"]/player_obj["Total Games"])*100+"% Scissors: "+(npc_obj["scissors"]/player_obj["Total Games"])*100+"%";
 
 }
